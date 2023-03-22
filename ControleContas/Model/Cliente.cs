9a0dbb1022b3dda83034389a27de4bce2979d33b
KLD17 <kylderypereira@gmail.com>
2023-03-22 20:43:25 -0300

    validação cliente sobre dadosok

--- ControleContas/Model/Cliente.cs
+++ ControleContas/Model/Cliente.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace ControleContas.Model
 {
-    internal class Cliente
+    public class Cliente
     {
         public string Nome { get; private set; }
 
         public string Cpf { get; private set; }
         
-        public int AnoNascimento { get; private set; } 
+        public int AnoNascimento { get; private set; }
+
+        public Cliente(string nome, string cpf, int anoNascimento)
+        {
+            if (2023 - anoNascimento < 18)
+                throw new Exception("O Cliente deve ser maior de idade");
+            if (cpf.Length != 11)
+                throw new Exception("CPF invalido,deve conter 11 digitos, sem pontos e traços.");
+            Nome = nome;
+            Cpf = cpf;
+            AnoNascimento = anoNascimento;
+        }
     }
+
+
+
+
 }
